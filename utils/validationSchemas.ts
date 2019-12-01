6aef35a2de267bda988fed8310d782e966675706
R Sánchez <rene.as.himself@gmail.com>
2019-11-30 23:28:09 -0600

    Username must start with letter

--- utils/validationSchemas.ts
+++ utils/validationSchemas.ts
@@ -6,30 +6,31 @@ export const currencyValidation = yup.string().matches(/^\$?(\d{1,3})(\,?\d{3})*
 export const amountValidation = yup.string().matches(/(\d)*\.?\d{0,2}/, 'Enter valid amount')
 
 export const usernameValidation = yup
-    .string()
-    .min(3, 'Username must be at least 3 characters long.')
-    .max(25, 'Username must be max. 25 characters.')
-    .required('Username is required.')
+  .string()
+  .min(3, 'Username must be at least 3 characters long.')
+  .max(25, 'Username must be max. 25 characters.')
+  .matches(/^[a-zA-Z]/, 'Username must start with a letter.')
+  .required('Username is required.')
 
 export const emailValidation = yup
-    .string()
-    .email('Invalid email.')
-    .required('Email is required.')
+  .string()
+  .email('Invalid email.')
+  .required('Email is required.')
 
 export const minLength: number = 6
 export const maxLength: number = 25
 
 export const passwordValidation = yup
-    .string()
-    .min(minLength, `Password must be at least ${minLength} characters long.`)
-    .max(maxLength, `Password must be max. ${maxLength} characters.`)
-    // .matches(
-    //     /^(?=.*[A-Za-z])(?=.*d)(?=.*[@$!%*#?&])[A-Za-zd@$!%*#?&]{10,}$/,
-    //     'Must Contain 10 Characters, One Uppercase, One Lowercase, One Number and one special case Character'
-    // )
-    .required('Password is required.')
+  .string()
+  .min(minLength, `Password must be at least ${minLength} characters long.`)
+  .max(maxLength, `Password must be max. ${maxLength} characters.`)
+  // .matches(
+  //     /^(?=.*[A-Za-z])(?=.*d)(?=.*[@$!%*#?&])[A-Za-zd@$!%*#?&]{10,}$/,
+  //     'Must Contain 10 Characters, One Uppercase, One Lowercase, One Number and one special case Character'
+  // )
+  .required('Password is required.')
 
 export const confirmPasswordValidation = yup
-    .string()
-    .oneOf([yup.ref('password'), null], 'Passwords must match.')
-    .required('Confirm Password is required.')
+  .string()
+  .oneOf([yup.ref('password'), null], 'Passwords must match.')
+  .required('Confirm Password is required.')
