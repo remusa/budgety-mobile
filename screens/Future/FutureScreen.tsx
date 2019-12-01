8ae53900ad0324809963b5cfec0c80150aa5b562
R Sánchez <rene.as.himself@gmail.com>
2019-11-30 23:28:33 -0600

    Formatting

--- screens/Future/FutureScreen.tsx
+++ screens/Future/FutureScreen.tsx
@@ -1,33 +1,33 @@
 import React from 'react'
 import { StyleSheet } from 'react-native'
-import { Button, Layout, Text } from 'react-native-ui-kitten'
+import { Layout, Text } from 'react-native-ui-kitten'
 import { FIREBASE_SIGNOUT } from '../../utils/auth'
 
 interface Props {}
 
 const FutureScreen: React.FC<Props> = props => {
-    const handleLogout = () => {
-        FIREBASE_SIGNOUT().then(() => {
-            props.navigation.navigate('Auth')
-        })
-    }
+  const handleLogout = () => {
+    FIREBASE_SIGNOUT().then(() => {
+      props.navigation.navigate('Auth')
+    })
+  }
 
-    return (
-        <Layout style={styles.container}>
-            <Text category="h1">Future</Text>
+  return (
+    <Layout style={styles.container}>
+      <Text category='h1'>Future</Text>
 
-            <Button onPress={handleLogout}>Logout</Button>
-        </Layout>
-    )
+      {/* <Button onPress={handleLogout}>Logout</Button> */}
+    </Layout>
+  )
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: 'flex-start',
-        alignItems: 'center',
-        height: '100%',
-    },
+  container: {
+    flex: 1,
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+    height: '100%',
+  },
 })
 
 export default FutureScreen
--- screens/PastMonths/PastMonthsScreen.tsx
+++ screens/PastMonths/PastMonthsScreen.tsx
@@ -1,33 +1,33 @@
 import React from 'react'
 import { StyleSheet } from 'react-native'
-import { Button, Layout, Text } from 'react-native-ui-kitten'
+import { Layout, Text } from 'react-native-ui-kitten'
 import { FIREBASE_SIGNOUT } from '../../utils/auth'
 
 interface Props {}
 
-const PastMonthsScreen: React.FC<Props> = props => {
-    const handleLogout = () => {
-        FIREBASE_SIGNOUT().then(() => {
-            props.navigation.navigate('Auth')
-        })
-    }
+const LastMonthsScreen: React.FC<Props> = props => {
+  const handleLogout = () => {
+    FIREBASE_SIGNOUT().then(() => {
+      props.navigation.navigate('Auth')
+    })
+  }
 
-    return (
-        <Layout style={styles.container}>
-            <Text category="h1">Past Months</Text>
+  return (
+    <Layout style={styles.container}>
+      <Text category='h1'>Past Months</Text>
 
-            <Button onPress={handleLogout}>Logout</Button>
-        </Layout>
-    )
+      {/* <Button onPress={handleLogout}>Logout</Button> */}
+    </Layout>
+  )
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: 'flex-start',
-        alignItems: 'center',
-        height: '100%',
-    },
+  container: {
+    flex: 1,
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+    height: '100%',
+  },
 })
 
-export default PastMonthsScreen
+export default LastMonthsScreen
