04cb3962bda23dd2293a6df0123109c45bbe8977
R Sánchez <rene.as.himself@gmail.com>
2019-12-01 18:24:14 -0600

    Import firebase from firebase/app

--- utils/Firebase.ts
+++ utils/Firebase.ts
@@ -1,22 +1,22 @@
-import firebase from 'firebase'
+import firebase from 'firebase/app'
 import 'firebase/firestore'
 import {
-    API_KEY,
-    APP_ID,
-    AUTH_DOMAIN,
-    DATABASE_URL,
-    MESSAGE_SENDER_ID,
-    PROJECT_ID,
+  API_KEY,
+  APP_ID,
+  AUTH_DOMAIN,
+  DATABASE_URL,
+  MESSAGE_SENDER_ID,
+  PROJECT_ID,
 } from 'react-native-dotenv'
 
 const firebaseConfig = {
-    apiKey: API_KEY,
-    authDomain: AUTH_DOMAIN,
-    databaseURL: DATABASE_URL,
-    projectId: PROJECT_ID,
-    storageBucket: '',
-    messagingSenderId: MESSAGE_SENDER_ID,
-    appId: APP_ID,
+  apiKey: API_KEY,
+  authDomain: AUTH_DOMAIN,
+  databaseURL: DATABASE_URL,
+  projectId: PROJECT_ID,
+  storageBucket: '',
+  messagingSenderId: MESSAGE_SENDER_ID,
+  appId: APP_ID,
 }
 
 // Initialize Firebase
